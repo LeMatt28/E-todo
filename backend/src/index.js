fcebfb286e6b0ab959cedfe3540713f4467e6052
Matt <matteo.castronvo@epitech.eu>
2025-11-13 13:25:25 +0100

    token

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,44 +16,10 @@ const db = require("./config/db")
 const reg = require("./routes/register/reg")
 const user = require("./routes/user/users") 
 const login = require("./routes/auth/auth")
+const token = require("./middleware/auth")
 app.use("/register", reg) 
 app.use("/user", token, user)
 app.use("/login", login)
-
-//CRÉE UN TOKEN
-const jwt = require("jsonwebtoken")
-
-
-const secret = process.env.SECRET; 
-
-app.post("/generaterToken", (req,res)=>{
-    const {email,password} = req.body
-
-    const token = jwt.sign({email,password},secret,{expiresIn:'72h'})
-
-    console.log(token,"token")
-    res.status(200).send({message:"generated succesfully", token}) //fait appel à ma fonction token (idem token:token)
-
-})
-
-// VERIFIER LE TOKEN 
-
-function token (req, res, next) { //verifie l'itinairaire
-    try {
-        const token  = req.header.token; //obtenir la valeur du token ?
-        if (token){
-            const decode = jwt.verify(token, secret); //verifie le jeton
-
-            res.json("tu passe");
-            next();
-
-        } else { //réponse négative, donc erreur
-            res.json("bah nn tu passe pas");
-        }
-    } catch(err) {
-        console.error(err.message)
-    }
-};
 
 // REPONSE DU SERVEUR
 app.get("/", (req,res) =>{
@@ -62,3 +28,4 @@ app.get("/", (req,res) =>{
 app.listen(port, () => {
     console.log("serveur ca marche le s")
 })
+
