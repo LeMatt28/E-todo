dbb601a52e87b7cace02a68cc5801ad244686044
Matt <matteo.castronvo@epitech.eu>
2025-11-17 10:22:47 +0100

    base sql push

---
+++ e-todo.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : lun. 17 nov. 2025 à 09:20
+-- Version du serveur : 8.0.43-0ubuntu0.24.04.2
+-- Version de PHP : 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `etodo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `todo`
+--
+
+CREATE TABLE `todo` (
+  `id` int NOT NULL,
+  `title` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `due_time` datetime NOT NULL,
+  `status` enum('not started','todo','in progress','done') DEFAULT 'not started',
+  `user_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `todo`
+--
+
+INSERT INTO `todo` (`id`, `title`, `description`, `created_at`, `due_time`, `status`, `user_id`) VALUES
+(1, 'po', 'zab', '2025-11-14 10:11:50', '2020-01-01 00:00:00', 'todo', 76);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `name`, `firstname`, `created_at`) VALUES
+(31, 'a@a', '$2b$10$nlX7jgFOb5UHlzT52FYJjO8F1luU9/R0ZzvzyqisLvdyKIb26F8oS', 'a', 'a', '2025-11-07 15:46:39'),
+(76, 'mpo@p', '$2b$10$if6MGGviToR7FILTRhXD7.H9xpjIrlDO2Xf7TbGui28Qshh36BgYu', 'a', 'a', '2025-11-14 09:54:23');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `todo`
+--
+ALTER TABLE `todo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `todo`
+--
+ALTER TABLE `todo`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `todo`
+--
+ALTER TABLE `todo`
+  ADD CONSTRAINT `todo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
