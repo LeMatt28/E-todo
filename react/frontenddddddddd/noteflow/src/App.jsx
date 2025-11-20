d625dd8cfe0c0eb7959604802107c3f96604369a
Matt <matteo.castronvo@epitech.eu>
2025-11-20 15:47:29 +0100

    oue

--- react/frontenddddddddd/noteflow/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-import Home from './pages/Home.jsx'
-import Dashboard from './Dashboard.jsx'
-
-export default function App(){
-  const [user, setUser] = useState(null)
-  if(!user) return <Home onLogin={setUser} />
-  return <Dashboard user={user} />
-}
